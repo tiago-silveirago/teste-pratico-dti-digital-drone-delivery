b696a9c7641e6debcd9a8917a75f6a3cac71d981
Tiago Silveira <tgosilveira@sga.pucminas.br>
2025-09-27 11:42:55 -0300

    chore: cria estrutura do projeto

---
+++ src/main/java/com/tiago_silveirago/testepraticodtidigitaldronedelivery/TestePraticoDtiDigitalDroneDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.testepraticodtidigitaldronedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoDtiDigitalDroneDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestePraticoDtiDigitalDroneDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiago_silveirago/testepraticodtidigitaldronedelivery/TestePraticoDtiDigitalDroneDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.testepraticodtidigitaldronedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoDtiDigitalDroneDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
